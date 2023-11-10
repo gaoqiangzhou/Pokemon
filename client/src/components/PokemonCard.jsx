c363c5ec9fe1b209bdd86397ca419d3195bdf960
Vinuk Ranaweera <vinukranaweera@gmail.com>
2023-11-10 03:17:32 -0500

    Created remove button

--- client/src/components/PokemonCard.jsx
+++ client/src/components/PokemonCard.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
-import TypeBadge from './TypeBadge'
+import React from 'react';
+import TypeBadge from './TypeBadge';
 
 const PokemonCard = (props) => {
-  return (
-    <div className="bg-gradient-to-r from-[#edfedb] to-[#92f91b] flex flex-row rounded-lg">
-      <img src = {props.spriteUrl}></img>
-      <div className="flex flex-col">
-        <span>{props.name}</span>
-        {
-          (props.types.length === 1) ? 
-          (<div><TypeBadge pokeType = {props.types[0]}/></div>)
-          : 
-          (<div><TypeBadge pokeType = {props.types[0]}/> / <TypeBadge pokeType = {props.types[1]}/></div>)
-        }
-      </div>
-    </div>
-  )
-}
+  const handleRemovePokemon = () => {
+    if (props.onRemove) {
+        props.onRemove(props.name);
+    }
+    };
+    return (
+        <div className="bg-gradient-to-r from-[#edfedb] to-[#92f91b] flex flex-row rounded-lg">
+            <img src={props.spriteUrl} alt={props.name} />
+            <div className="flex flex-col">
+                <span>{props.name}</span>
+                {
+                  (props.types.length === 1) ? 
+                    (<div><TypeBadge pokeType={props.types[0]} /></div>)
+                    : 
+                    (<div><TypeBadge pokeType={props.types[0]} /> / <TypeBadge pokeType={props.types[1]} /></div>) 
+                }
+            </div>
+            <button
+                className="bg-red-500 text-white rounded-full w-6 h-6 flex items-center justify-center"
+                onClick={handleRemovePokemon}
+            >
+                X
+            </button>
+        </div>
+    );
+};
 
-export default PokemonCard
+export default PokemonCard;
+
+
+
